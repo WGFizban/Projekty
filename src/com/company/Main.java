ff05888106a9ea7472d8696017ae20069d888ff7
WGFizban <63311150+WGFizban@users.noreply.github.com>
2020-04-07 21:24:07 +0200

    my reposytory

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal dog = new Animal("Dog");
+
+        dog.name="Haiko";
+
+
+        System.out.println("I am "+dog.name);
+
+        dog.feed();
+        dog.feed();
+        dog.feed();
+
+
+        Human me = new Human();
+        me.firstName="Wojo";
+        me.lastName="Graban";
+        me.pet = dog;
+
+
+
+
+
+    }
+}
