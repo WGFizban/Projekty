66b9e9cc4a0096faa314b997b73d37e45c3e0fb6
WGFizban <63311150+WGFizban@users.noreply.github.com>
2020-04-18 20:46:50 +0200

    zad 2

---
+++ src/com/company/Car.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Car {
+    String model;
+    String producer;
+    Integer year;
+
+    public Car(String model, String producer, Integer year) {
+        this.model = model;
+        this.producer = producer;
+        this.year = year;
+    }
+
+
+}
+
